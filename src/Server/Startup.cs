31250c18c996832ba3666ec44e0d7794079f389e
Kristian Mariyanov <kmariyan@progress.com>
2024-11-03 23:29:18 +0200

    start migration to net8

--- src/Server/Startup.cs
+++ src/Server/Startup.cs
@@ -22,9 +22,8 @@ namespace KristianCode.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllersWithViews();
-            services.AddRazorPages();
+            services.AddControllers();
+            services.AddRazorComponents().AddInteractiveWebAssemblyComponents();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,17 +41,15 @@ namespace KristianCode.Server
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
-            app.UseBlazorFrameworkFiles();
-            app.UseStaticFiles();
+            app.UseHttpsRedirection();       
 
             app.UseRouting();
+            app.UseAntiforgery();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
                 endpoints.MapControllers();
-                endpoints.MapFallbackToFile("index.html");
+                endpoints.MapRazorComponents<KristianCode.Client.Host>().AddInteractiveWebAssemblyRenderMode();
             });
         }
     }
